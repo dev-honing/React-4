cf4e532763021ef8b8a1c73a2021820e3a5787a3
Ho Nyeong <dev.honing@gmail.com>
2023-11-14 11:25:05 +0900

    클릭 이벤트 생성

--- app.js
+++ app.js
@@ -5,3 +5,13 @@ let stateData = {
 // id를 지정한 요소들에 접근하기 위해 변수 선언
 const button = document.getElementById("toggle-button");
 const displayText = document.getElementById("display-text");
+
+// 클릭 이벤트 생성
+button.addEventListener("click", function () {
+  if ( stateData.isActive ){
+    stateData.isActive = false;
+  } else {
+    stateData.isActive = true;
+  }
+  updateUI();
+})
