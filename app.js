fc6f4b346651491caf9b6bef0c164c9634327693
Ho Nyeong <dev.honing@gmail.com>
2023-11-14 10:49:59 +0900

    요소들에 접근하기 위한 변수 선언

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('hello');
+let stateData = {
+  isActive: false
+};
+
+// id를 지정한 요소들에 접근하기 위해 변수 선언
+const button = document.getElementById("toggle-button");
+const displayText = document.getElementById("display-text");
